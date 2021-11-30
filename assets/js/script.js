0dbcc8dc4cc7a98b6447f4fb267fbdfedab7d05f
StitchCodes <coderstitch@gmail.com>
2021-11-29 18:03:48 -0600

    Adding function pseudo code for all different functions, changed html to match ids on js. Added js proper elemnt ids. Added mockup for question and score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,36 @@
 // BOTON DE INICIAR EL QUIZ QUE EJECUTA EL PROGRAMA
 // INICIAR EL TIMER
 // CAMBIAR EL TEXTO POR UNA PREGUNTA
-// CREAR PREGUNTAS
+
 // CREAR RESPUESTA PARA CADA PREGUNTA
 // COMPARAR LA RESPUESTA DEL USUARIO CONTRA LA CORRECTA
 // DAR UNA PUNTUACION Y PASAR A LA SIGUIENTE PREGUNTA
 // AL FINAL DAR PUNTUACION Y MOSTRAR BOX PARA REGISTRAR EL NOMBRE
 // REGISTRAR EL NOMBRE EN LOCALSTORAGE
 // VER LOS NOMBRES EN LA PAGINA DE HIGHSCORES
-// BOTON PARA VOLVER A JUGAR 
-// BOTON PARA LIMIPAR EL HIGHSCORE
+
+
 
 // GLOBAL VARIABLES
 // GET ELEMENTS
 
-var countdwn = document.getElementById("countdown");
-var mainEl = document.getElementById("main-section");
 var mainText = mainEl.querySelectorAll("h1");
-var startBtn = mainEl.getElementById("startGameBtn");
+var countdwn = document.getElementById("#countdown");
+var mainEl = document.getElementById("#main-section");
+var startBtn = mainEl.getElementById("#startGameBtn");
 
 console.log("coutndown", countdwn);
 console.log("main section", mainEl);
 console.log("h1 text", mainText);
 
-
+// CREAR PREGUNTAS
+var questionOne = {
+    Question:"Are you happy?", 
+    option1:"Yes", 
+    option2:"No",
+    option3:"dunno",
+    option4:"maybe"
+};
 // ON CLICK EVENT
 startBtn.addEventListener("click", gameStart());
 
@@ -35,3 +42,14 @@ function gameStart() {
 }
 
 countdwn.textContent = "Time left: 4 seconds";
+
+
+// BOTON PARA LIMIPAR EL HIGHSCORE
+function clearHighscore() {
+    console.log("highscore cleared");
+}
+
+// BOTON PARA VOLVER A JUGAR 
+function replay() {
+    console.log("I want to play again");
+}
