9dbdef780220bf67888d229ab6550493f5f7bfba
StitchCodes <coderstitch@gmail.com>
2021-11-29 21:04:20 -0600

    Added neew question and function to call for next question. Code is not optimized at all

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,20 +15,13 @@ var mainSection = document.getElementById("main-section");
 var mainText = mainSection.querySelectorAll("h1");
 var startBtn = document.getElementById("startGameBtn");
 var countdwn = document.getElementById("countdown");
+var score = 0;
 
 
 // CREAR PREGUNTAS
-var questionOne = {
-    Question:"Are you happy?", 
-    option1:"Yes", 
-    option2:"No",
-    option3:"dunno",
-    option4:"maybe"
-};
-
 var questions = {
     question1: {
-        question: "are you happy?",
+        question: "Are you happy?",
         option1: "Yes", 
         option2: "No",
         option3: "dunno",
@@ -39,8 +32,8 @@ var questions = {
         question: "Are you married?",
         option1: "Yes", 
         option2: "No",
-        option3: "dunno",
-        option4: "maybe"
+        option3: "Long Ago",
+        option4: "Obvs!"
     }
 };
 
@@ -51,7 +44,6 @@ startBtn.addEventListener("click", gameStart);
 // FUNCTIONS
 // START GAME FUNCTION
 function gameStart() {
-    console.log("Game Will start soon.")
     // Add question to DOM
     mainSection.innerHTML = "<h1>" + questions.question1.question;
     // Create ol
@@ -92,8 +84,6 @@ function gameStart() {
 
     // Compare question
     var qstnTrigger = document.getElementsByClassName("questions");
-    console.log("qstn", qstnTrigger);
-    // qstnTrigger.addEventListener("click", compare);
 
     for (let i = 0; i < qstnTrigger.length; i++) {
         qstnTrigger[i].addEventListener("click", compare);
@@ -106,14 +96,76 @@ function gameStart() {
 
 // Compare function
 function compare(event) {
-    tgt = event.tgt;
+    tgt = event.target;
 
-    console.log("Triggered Event Target", tgt);
+    if (tgt !== "Yes") {
+        console.log("tgt", tgt);
+        alert("0 points!");
+        score = 0;
+        nextQuestion();
+    }   else {
+        alert("Gryffondor +10 points!");
+        score = score + 10;
+        nextQuestion();
+    }
+}
+
+// **************************************************************************************
+// Next Question Function
+function nextQuestion() {
+    // Add new question
+    mainSection.innerHTML = "<h1>" + questions.question2.question
+
+    // Create List
+    var createList = document.createElement("ol");
+    var listItems1 = document.createElement("li");
+    listItems1.innerHTML = questions.question2.option1;
+    var listItems2 = document.createElement("li");
+    listItems2.innerHTML = questions.question2.option2;
+    var listItems3 = document.createElement("li");
+    listItems3.innerHTML = questions.question2.option3;
+    var listItems4 = document.createElement("li");
+    listItems4.innerHTML = questions.question2.option4;
+
+    // Add class to each li
+    listItems1.setAttribute("class", "questions");
+    listItems2.setAttribute("class", "questions");
+    listItems3.setAttribute("class", "questions");
+    listItems4.setAttribute("class", "questions");
+
+    // Append each li
+    createList.appendChild(listItems1);
+    createList.appendChild(listItems2);
+    createList.appendChild(listItems3);
+    createList.appendChild(listItems4);
     
+    // Send List to DOM
+    mainSection.appendChild(createList);
+    // Create Buttons to DOM
+    var quitbtn = document.createElement("button");
+    // quitbtn.setAttribute("id", "button");
+    quitbtn.innerHTML = "Quit"
+    mainSection.appendChild(quitbtn);
+
+    var nextBtn = document.createElement("button");
+    nextBtn.innerHTML = "Next";
+    mainSection.appendChild(nextBtn);
+
+    // Compare question
+    var qstnTrigger = document.getElementsByClassName("questions");
+
+    for (let i = 0; i < qstnTrigger.length; i++) {
+        qstnTrigger[i].addEventListener("click", compare);
+        
+    }
 }
 
 countdwn.textContent = "Time left: 4 seconds";
 
+// Timmer function
+function timer() {
+    
+}
 
 // BOTON PARA LIMIPAR EL HIGHSCORE
 function clearHighscore() {
